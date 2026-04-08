81bc8e0a73cf9142eb5908a5bc40e341e3469f27
SQB <u@sqb.uz>
2026-04-08 16:24:10 +0500

    Remove Қайтган мигрантлар card + fix KPI refs (34-37)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1240,10 +1240,10 @@ function kpiFromIndicator(data, no, opts){
 }
 
 const REGION_KPI_DEFS = [
-  {no:36, icon:"bi-geo-alt-fill",        labelKey:"kpi_mahalla",   unitKey:"unit_ta"},
-  {no:38, icon:"bi-house-door-fill",     labelKey:"kpi_xonadon",   unit:""},
-  {no:37, icon:"bi-people",              labelKey:"kpi_oila",      unit:""},
-  {no:35, icon:"bi-people-fill",         labelKey:"kpi_aholi",     unitKey:"unit_ming_kishi"},
+  {no:35, icon:"bi-geo-alt-fill",        labelKey:"kpi_mahalla",   unitKey:"unit_ta"},
+  {no:37, icon:"bi-house-door-fill",     labelKey:"kpi_xonadon",   unit:""},
+  {no:36, icon:"bi-people",              labelKey:"kpi_oila",      unit:""},
+  {no:34, icon:"bi-people-fill",         labelKey:"kpi_aholi",     unitKey:"unit_ming_kishi"},
   {no:27, icon:"bi-person-x-fill",       labelKey:"kpi_ishsizlik", unit:"%", pctCount:true},
   {no:25, icon:"bi-arrow-down-circle",   labelKey:"kpi_kambag",    unit:"%", pctCount:true},
 ];
@@ -1307,9 +1307,9 @@ function buildRegionInsights(district){
   const tpl = AI_TPL[STATE.lang] || AI_TPL.uz;
   const out = [];
 
-  const mah = kpiFromIndicator(data, 36, {});
-  const xon = kpiFromIndicator(data, 38, {});
-  const oila = kpiFromIndicator(data, 37, {});
+  const mah = kpiFromIndicator(data, 35, {});
+  const xon = kpiFromIndicator(data, 37, {});
+  const oila = kpiFromIndicator(data, 36, {});
   const ishsiz = kpiFromIndicator(data, 27, {});
   const kamb = kpiFromIndicator(data, 25, {});
 
